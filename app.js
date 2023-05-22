1d2a7101359c89fbe5f5acb2b7c0fead02af555e
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-05-22 11:19:53 +0200

    exercise done

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -11,9 +12,12 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const users = require("./users");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", users.getUsers);
+app.get("/api/users/:id", users.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
