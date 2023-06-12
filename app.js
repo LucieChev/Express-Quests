1a4a43439bb17b96ac9705129bc59826b5089a32
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-06-12 16:57:34 +0200

    express 08

--- app.js
+++ app.js
@@ -1,39 +1,28 @@
 require("dotenv").config();
+app.use(express.json());
 
 const express = require("express");
 const port = process.env.APP_PORT ?? 5000;
 const movieHandlers = require("./movieHandlers");
-
 const app = express();
 const userHandlers = require("./userHandlers");
 const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
-
-app.use(express.json());
-
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
-
 app.post(
   "/api/login",
   userHandlers.getUserByEmailWithPasswordAndPassToNext,
   verifyPassword
 );
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
+
 app.use(verifyToken);
 
 app.post("/api/movies", movieHandlers.postMovie);
@@ -41,3 +30,11 @@ app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
