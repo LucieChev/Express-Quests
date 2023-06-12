9d69a8cd66800ae4fc9f6bd0bf05bcc91f5ac7d4
LucieChev <123562385+LucieChev@users.noreply.github.com>
2023-06-12 16:54:30 +0200

    express 08

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", hashPassword, userHandlers.postUser);
 
 app.post(
   "/api/login",
@@ -38,6 +39,5 @@ app.use(verifyToken);
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
